f5e841d7e7efd94d515e04fb5488a8b4825da149
joon0447 <94978051+joon0447@users.noreply.github.com>
2023-03-11 21:08:44 +0900

    Create solution.js

---
+++ level-0/소인수분해/solution.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let answer = [];  
+    let i = 2;
+    while (n!==1){
+        if(n % i === 0){
+            answer.push(i);
+            n /= i;
+        }   
+        else i++;
+    }
+    return Array.from(new Set(answer));
+}
